82ba96d2e69d6cc8df3f44f06e68079ce5779e1a
Mattia Bruno <mattia.bruno.kzmz@gmail.com>
2021-04-04 17:12:41 +0200

    babylon integration

--- scripts/module/Foundry3D.js
+++ scripts/module/Foundry3D.js
@@ -1,5 +1,5 @@
 import { ECanvasPositions } from "./CanvasPositions.js";
-import {ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3} from "../libs/babylon.max.js";
+export const BABYLON = window.BABYLON;
 
 export class Foundry3D {
     static get DEFAULT_OPTIONS() {
@@ -26,7 +26,7 @@ export class Foundry3D {
     }
 
     _setupCanvas() {
-        this.canvas = $('<div id="foundry3d-canvas" style="position: absolute; left: 0; top: 0;pointer-events: none;"></div>');
+        this.canvas = $('<canvas id="foundry3d-canvas" style="position: absolute; left: 0; top: 0;pointer-events: none;"></canvas>');
         switch (Foundry3D.CONFIG.canvasPosition) {
             case ECanvasPositions.OVER_SCENE:
                 this.canvas.css('z-index', 1000);
@@ -41,7 +41,7 @@ export class Foundry3D {
     }
 
     _resizeCanvas() {
-        const sidebarWidth = $('#sidebar').width();
+        const sidebarWidth = $('#sidebar').width() + 10;
         this.canvas.width(window.innerWidth - sidebarWidth + 'px');
         this.canvas.height(window.innerHeight - 1 + 'px');
 
@@ -49,14 +49,14 @@ export class Foundry3D {
     }
 
     _setupEngine() {
-        this._engine = new Engine(this.canvas[0], true);
-        this._scene = new Scene(this._engine);
+        this._engine = new BABYLON.Engine(this.canvas[0], true);
+        this._scene = new BABYLON.Scene(this._engine);
 
-        const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        camera.attachControl(this.canvas, true);
+        const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), this._scene);
+        camera.attachControl(this.canvas[0], true);
 
-        const light = new HemisphericLight("Light", new Vector3(1, 1, 0), this._scene);
-        const sphere = MeshBuilder.CreateSphere("Sphere", { diameter: 1 }, this._scene);
+        const light = new BABYLON.HemisphericLight("Light", new BABYLON.Vector3(1, 1, 0), this._scene);
+        const sphere = BABYLON.MeshBuilder.CreateSphere("Sphere", { diameter: 1 }, this._scene);
 
         this._engine.runRenderLoop(() => {
             this._scene.render();
